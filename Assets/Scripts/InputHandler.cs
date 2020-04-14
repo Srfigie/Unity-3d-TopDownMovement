1c9de975fe3805b08ef9c40b4d15d94006db0357
Sam Figie <SamFigie@gmail.com>
2020-04-13 20:08:27 -0500

    Solution

---
+++ Assets/Scripts/InputHandler.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputHandler : MonoBehaviour
+{
+    public Vector2 InputVector { get; private set; }
+
+    public Vector3 MousePosition { get; private set; }
+    // Update is called once per frame
+    void Update()
+    {
+        var h = Input.GetAxis("Horizontal");
+        var v = Input.GetAxis("Vertical");
+        InputVector = new Vector2(h, v);
+
+        MousePosition = Input.mousePosition;
+    }
+}
---
+++ Assets/Scripts/TopDownCharacterMover.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(InputHandler))]
+public class TopDownCharacterMover : MonoBehaviour
+{
+    private InputHandler _input;
+
+    [SerializeField]
+    private bool RotateTowardMouse;
+
+    [SerializeField]
+    private float MovementSpeed;
+    [SerializeField]
+    private float RotationSpeed;
+
+    [SerializeField]
+    private Camera Camera;
+
+    private void Awake()
+    {
+        _input = GetComponent<InputHandler>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        var targetVector = new Vector3(_input.InputVector.x, 0, _input.InputVector.y);
+        var movementVector = MoveTowardTarget(targetVector);
+
+        if (!RotateTowardMouse)
+        {
+            RotateTowardMovementVector(movementVector);
+        }
+        if (RotateTowardMouse)
+        {
+            RotateFromMouseVector();
+        }
+
+    }
+
+    private void RotateFromMouseVector()
+    {
+        Ray ray = Camera.ScreenPointToRay(_input.MousePosition);
+
+        if (Physics.Raycast(ray, out RaycastHit hitInfo, maxDistance: 300f))
+        {
+            var target = hitInfo.point;
+            target.y = transform.position.y;
+            transform.LookAt(target);
+        }
+    }
+
+    private Vector3 MoveTowardTarget(Vector3 targetVector)
+    {
+        var speed = MovementSpeed * Time.deltaTime;
+        // transform.Translate(targetVector * (MovementSpeed * Time.deltaTime)); Demonstrate why this doesn't work
+        //transform.Translate(targetVector * (MovementSpeed * Time.deltaTime), Camera.gameObject.transform);
+
+        targetVector = Quaternion.Euler(0, Camera.gameObject.transform.rotation.eulerAngles.y, 0) * targetVector;
+        var targetPosition = transform.position + targetVector * speed;
+        transform.position = targetPosition;
+        return targetVector;
+    }
+
+    private void RotateTowardMovementVector(Vector3 movementDirection)
+    {
+        if(movementDirection.magnitude == 0) { return; }
+        var rotation = Quaternion.LookRotation(movementDirection);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, RotationSpeed);
+    }
+}
