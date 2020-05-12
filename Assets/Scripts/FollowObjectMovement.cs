1c641fd779cd05356228bb99d10bc070e01ef205
Sam Figie <SamFigie@gmail.com>
2020-05-11 19:19:25 -0500

    minimap-start

---
+++ Assets/Scripts/FollowObjectMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowObjectMovement : MonoBehaviour
+{
+    public Transform TargetTransform;
+
+    private float xOffset;
+    private float yOffset;
+    private float zOffset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        xOffset = transform.position.x - TargetTransform.position.x;
+        yOffset = transform.position.y - TargetTransform.position.y;
+        zOffset = transform.position.z - TargetTransform.position.z;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.position = new Vector3(
+            TargetTransform.position.x + xOffset,
+            TargetTransform.position.y + yOffset,
+            TargetTransform.position.z + zOffset);
+        
+    }
+}
--- Assets/Scripts/TopDownCharacterMover.cs
+++ Assets/Scripts/TopDownCharacterMover.cs
@@ -3,50 +3,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(InputHandler))]
 public class TopDownCharacterMover : MonoBehaviour
 {
     private InputHandler _input;
 
     [SerializeField]
-    private bool RotateTowardMouse;
+    private float moveSpeed;
+    [SerializeField]
+    private float rotateSpeed;
+    [SerializeField]
+    private bool rotateTowardsMouse;
 
     [SerializeField]
-    private float MovementSpeed;
-    [SerializeField]
-    private float RotationSpeed;
-
-    [SerializeField]
-    private Camera Camera;
+    private Camera camera;
 
     private void Awake()
     {
         _input = GetComponent<InputHandler>();
     }
 
+
     // Update is called once per frame
     void Update()
     {
-        
         var targetVector = new Vector3(_input.InputVector.x, 0, _input.InputVector.y);
+
         var movementVector = MoveTowardTarget(targetVector);
-
-        if (!RotateTowardMouse)
-        {
+        if (!rotateTowardsMouse)
             RotateTowardMovementVector(movementVector);
-        }
-        if (RotateTowardMouse)
-        {
-            RotateFromMouseVector();
-        }
-
+        else
+            RotateTowardMouseVector();
     }
 
-    private void RotateFromMouseVector()
+    private void RotateTowardMouseVector()
     {
-        Ray ray = Camera.ScreenPointToRay(_input.MousePosition);
+        Ray ray = camera.ScreenPointToRay(_input.MousePosition);
 
-        if (Physics.Raycast(ray, out RaycastHit hitInfo, maxDistance: 300f))
+        if(Physics.Raycast(ray, out RaycastHit hitInfo, maxDistance: 300f))
         {
             var target = hitInfo.point;
             target.y = transform.position.y;
@@ -54,22 +47,21 @@ public class TopDownCharacterMover : MonoBehaviour
         }
     }
 
+    private void RotateTowardMovementVector(Vector3 movementVector)
+    {
+        if(movementVector.magnitude == 0) { return; }
+
+        var rotation = Quaternion.LookRotation(movementVector);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, rotateSpeed * Time.deltaTime);
+    }
+
     private Vector3 MoveTowardTarget(Vector3 targetVector)
     {
-        var speed = MovementSpeed * Time.deltaTime;
-        // transform.Translate(targetVector * (MovementSpeed * Time.deltaTime)); Demonstrate why this doesn't work
-        //transform.Translate(targetVector * (MovementSpeed * Time.deltaTime), Camera.gameObject.transform);
+        var speed = moveSpeed * Time.deltaTime;
 
-        targetVector = Quaternion.Euler(0, Camera.gameObject.transform.rotation.eulerAngles.y, 0) * targetVector;
+        targetVector = Quaternion.Euler(0, camera.gameObject.transform.eulerAngles.y, 0) * targetVector;
         var targetPosition = transform.position + targetVector * speed;
         transform.position = targetPosition;
         return targetVector;
-    }
-
-    private void RotateTowardMovementVector(Vector3 movementDirection)
-    {
-        if(movementDirection.magnitude == 0) { return; }
-        var rotation = Quaternion.LookRotation(movementDirection);
-        transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, RotationSpeed);
     }
 }
